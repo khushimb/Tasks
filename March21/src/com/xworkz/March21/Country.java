e22156866210fd666da7f5e723fd5a4fccb7659e
khushimb <mbkhushi12@gmail.com>
2025-03-24 08:38:48 +0530

    "uploaded"

---
+++ March21/src/com/xworkz/March21/Country.java
@@ -0,0 +1,19 @@
+package com.xworkz.March21;
+
+public class Country {
+    PrimeMinister primeMinister;
+    Minister minister;
+
+
+    public Country(PrimeMinister primeMinister, Minister minister) {
+        this.primeMinister = primeMinister;
+        this.minister=minister;
+    }
+     display()
+    {
+PrimeMinister primeMinister = new PrimeMinister("china",35000,49,"male");
+Minister minister= new Minister();
+    }
+}
+
+
